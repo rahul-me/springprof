fdbb88a52422c4cfa1ca12591b016e580a5397d0
Rahul Chauhan <chauhanr@pmc.local>
2021-12-18 18:52:49 +0530

    environment resources property and profiles

--- src/main/java/com/rvcode/pdfinvo/context/PdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/rvcode/pdfinvo/context/PdfInvoicesApplicationConfiguration.java
@@ -1,26 +1,17 @@
 package com.rvcode.pdfinvo.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rvcode.pdfinvo.ApplicationLauncher;
 import com.rvcode.pdfinvo.service.InvoiceService;
 import com.rvcode.pdfinvo.service.UserService;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.context.annotation.*;
 
 @Configuration
+@ComponentScan(basePackageClasses = ApplicationLauncher.class)
+@PropertySource("classpath:/application.properties")
+@PropertySource(value = "classpath:/application-${spring.profiles.active}.properties", ignoreResourceNotFound = true)
 public class PdfInvoicesApplicationConfiguration {
-    @Bean
-    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public UserService userService() {
-        return new UserService();
-    }
-
-    @Bean
-    public InvoiceService invoiceService(UserService userService) {
-        return new InvoiceService(userService);
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
--- src/main/java/com/rvcode/pdfinvo/service/UserService.java
+++ src/main/java/com/rvcode/pdfinvo/service/UserService.java
@@ -1,9 +1,11 @@
 package com.rvcode.pdfinvo.service;
 
 import com.rvcode.pdfinvo.model.User;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@Component
 public class UserService {
     public User findById(String id){
         String name = UUID.randomUUID().toString();
