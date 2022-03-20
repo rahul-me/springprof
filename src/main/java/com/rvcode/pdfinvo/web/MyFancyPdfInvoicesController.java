ed21f2143c02199585008a55c6cb12afea49da66
unknown <crahul10101991@gmail.com>
2022-03-20 09:49:16 +0530

    controller

--- src/main/java/com/rvcode/pdfinvo/web/MyFancyPdfInvoicesController.java
+++ src/main/java/com/rvcode/pdfinvo/web/MyFancyPdfInvoicesController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 
 import com.rvcode.pdfinvo.dto.InvoiceDto;
 import com.rvcode.pdfinvo.model.Invoice;
